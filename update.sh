e280f3dfca4d51b0cd84f5076976354c92396712
Zoey <zoey@z0ey.de>
2023-11-11 15:53:50 +0100

    Update update.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- update.sh
+++ update.sh
@@ -5,64 +5,101 @@ if [ -z "$TZ" ] || ! echo "$TZ" | grep -q "^[A-Za-z/]\+$"; then
     sleep inf
 fi
 
-if [ -z "$DUK" ] || ! echo "$DUK" | grep -q "^[A-Za-z0-9]\+$"; then
-    echo "DUK is unset or invalid, it can consist of upper letters A-Z, lower letters a-z and numbers 0-9."
+if [ -z "$CI" ] ||! echo "$CI" | grep -q "^[0-9]\+[smhd]\?$"; then
+    echo "CI needs to be a number which can be followed by one of the chars s, m, h or d."
     sleep inf
 fi
 
-
-if [ "$IPv4" = "false" ] || [ "$(curl -s4 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-        echo "IPv4 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv4" != "false" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-      echo "IPv4 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv4 does not work, disabling it."
-    export IPv4="false"
-  fi
-fi
-
-
-if [ "$IPv6" = "false" ] || [ "$(curl -s6 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-        echo "IPv6 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv6" != "false" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-      echo "IPv6 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv6 does not work, disabling it."
-    export IPv6="false"
-  fi
-fi
-
-
-if ! echo "$UI" | grep -q "^[0-9]\+[smhd]\?$"; then
-    echo "UI needs to be a number which can be followed by one of the chars s, m, h or d."
+if [ -z "$AID" ] || ! echo "$AID" | grep -q "^[A-Z0-9]\+$"; then
+    echo "AID is unset or invalid."
     sleep inf
 fi
 
-if [ "$IPv4" = "false" ] && [ "$IPv6" = "false" ]; then
-    echo "IPv4 and IPv6 disabled, stopping."
+if [ -z "$AAK" ] || ! echo "$AAK" | grep -q "^[A-Za-z0-9]\+$"; then
+    echo "AAK is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$AA" ] || ! echo "$AA" | grep -q "^[0-9]\+$"; then
+    echo "AA is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$AJ" ] || ! echo "$AJ" | grep -q "^[0-9]\+$"; then
+    echo "AJ is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$AC" ] || ! echo "$AC" | grep -q "^[0-9]\+$"; then
+    echo "AC is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$CUA" ] || ! echo "$CUA" | grep -q "^[A-Za-z0-9/().;: -]\+$"; then
+    echo "CUA is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$ACIDs" ] || ! echo "$ACIDs" | grep -q "^[A-Z ]\+$"; then
+    echo "ACIDs is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$ACAIIDs" ] || ! echo "$ACAIIDs" | grep -q "^[A-Z ]\+$"; then
+    echo "ACAIIDs is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$TBT" ] || ! echo "$TBT" | grep -q "^[A-Za-z0-9:]\+$"; then
+    echo "TBT is unset or invalid."
+    sleep inf
+fi
+
+if [ -z "$TCID" ] || ! echo "$TCID" | grep -q "^[0-9]\+$"; then
+    echo "TCID is unset or invalid."
     sleep inf
 fi
 
 
 while true; do
-    if [ "$IPv4" = "true" ]; then
-        curl -sLS4 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv4.json
-    fi
-    if [ "$IPv6" = "true" ]; then
-        curl -sLS6 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv6.json
-    fi
 
-    sleep "$UI"
+
+export message="\
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID" -A $CUA -H "x-api-key: $AAK" | jq -r .itinerary[].name) \
+ab \
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID" -A $CUA -H "x-api-key: $AAK" | jq -r .startDate) \n\
+"
+
+for ACID in $(echo "$ACIDs" | tr " " "\n"); do
+
+export message="\
+$message\
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIO%2CPREMIUM" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACID"'") | .priceModel') \
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIO%2CPREMIUM" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACID"'") | .head'): \
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIO%2CPREMIUM" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACID"'") | .price')€ \n\
+"
+
+done
+
+
+for ACAIID in $(echo "$ACAIIDs" | tr " " "\n"); do
+
+export message="\
+$message\
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIOAI%2CPREMIUMAI" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACAIID"'") | .priceModel') \
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIOAI%2CPREMIUMAI" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACAIID"'") | .head'): \
+$(curl -s "https://iris.cruise-api.aida.de/cruises/$AID?adults=$AA&juveniles=$AJ&children=$AC&priceModels=VARIOAI%2CPREMIUMAI" -A $CUA -H "x-api-key: $AAK" | jq -r '.cabinCategories[] | select(.id == "'"$ACAIID"'") | .price')€ \n\
+"
+
+done
+
+echo "$message"
+
+
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d '{"chat_id": "'"$TCID"'", "text": "'"$message"'", "disable_notification": true}' \
+     https://api.telegram.org/bot"$TBT"/sendMessage
+
+sleep "$CI"
 done
